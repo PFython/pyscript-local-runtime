690aab0ea7b5b229a0d13e917cba01d79702b1de
Pfython <peter@awsom.solutions>
2022-11-06 06:26:34 +0000

    Latest setup.py and runtime files

--- runtime/setup.py
+++ runtime/setup.py
@@ -27,8 +27,14 @@ if __name__ == "__main__":
         try:
             response = requests.get(url)
             filename = dir_path / url.split("/")[-1]
-            with open(filename,'wb') as f:
-                shutil.copyfileobj(response.raw, f)
+            with requests.get(url, stream=True) as r:
+                r.raise_for_status()
+                with open(filename, 'wb') as f:
+                    for chunk in r.iter_content(chunk_size=8192):
+                        # If you have chunk encoded response uncomment if
+                        # and set chunk_size parameter to None.
+                        #if chunk:
+                        f.write(chunk)
             Log.setup.info(f"Downloaded: {filename.name}")
         except Exception as E:
             Log.setup.error(f"Failed to download: {filename.name}\n{E}")
